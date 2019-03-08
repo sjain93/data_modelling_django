5865a372d439e6d8736ffd6b434f32391c086a59
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-03-07 19:00:27 -0500

    added models

--- project1/project1/models.py
+++ project1/project1/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+class Customer(models.Model):
+    name = models.CharField(max_length = 255)
+    mailing_address = models.CharField(max_length = 255)
+    email = models.EmailField(max_length =254)
+
+class Order(models.Model):
+    order_num = IntegerField()
+    date = DateField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name ='customer' )
